618f18ee6eaf692c87ef98ae5433197d76268d72
Cendeal <1798062051@qq.com>
2020-08-05 23:21:54 +0800

    [react-todo-list][cendeal]refactor: change timeout as 3000

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,27 +6,27 @@ import {message} from 'antd';
 
 const request = Axios.create({
     baseURL: 'https://5f2a43316ae5cc001642229d.mockapi.io/api/v1/',
-    timeout: 1000,
+    timeout: 3000,
     validateStatus: function (status) {
         return status >= 200 && status < 300; // default
     },
 
-})
+});
 
 request.interceptors.response.use(function (response) {
     return response.data;
 }, async function (error) {
     if (error.config.method.toUpperCase() !== 'GET') {
-        message.error(error.response ? error.response.data : 'unknown error')
-        const data = await getTodoList()
+        message.error(error.response ? error.response.data : 'unknown error');
+        const data = await getTodoList();
         store.dispatch(updateTodoList(data))
     }
     return Promise.reject(error);
 });
-export const GET = (url, data) => request.get(url, data)
-export const POST = (url, data) => request.post(url, data)
-export const PUT = (url, data) => request.put(url, data)
-export const DELETE = (url, data) => request.delete(url, data)
+export const GET = (url, data) => request.get(url, data);
+export const POST = (url, data) => request.post(url, data);
+export const PUT = (url, data) => request.put(url, data);
+export const DELETE = (url, data) => request.delete(url, data);
 export default {
     GET,
     POST,
